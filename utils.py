50bd4f92450ce6f2fd76224f8c389b900e22611a
FatimaEzzedinee <fatima_ezzeddine@outlook.com>
2023-10-29 19:00:53 +0100

    lab03

--- utils.py
+++ utils.py
@@ -227,3 +227,14 @@ class SolverTSP:
 def random_method(instance_): # TODO
     return np.random.choice(np.arange(instance_.nPoints), size=instance_.nPoints,
                             replace=False)
+
+
+def compute_length(solution, dist_matrix):
+    total_length = 0
+    starting_node = solution[0]
+    from_node = starting_node
+    for node in solution[1:]:
+        total_length += dist_matrix[from_node, node]
+        from_node = node
+    total_length += dist_matrix[starting_node, from_node]
+    return total_length
